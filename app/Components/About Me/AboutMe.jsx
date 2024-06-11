6befa25d422f911fd90e00524a522e19ceac6e85
unknown <sakibsafat47@gmail.com>
2024-06-11 07:56:01 -0700

    about me implement

---
+++ app/Components/About Me/AboutMe.jsx
@@ -0,0 +1,49 @@
+import Image from "next/image";
+import React from "react";
+import about from "../../../public/aboutme.png";
+import ButtonFull from "../buttons/ButtonFull";
+import "./aboutme.css"
+
+const AboutMe = () => {
+    const srslist=[
+        {
+            "title":"100%",
+            "des": "Project Completed"
+        },
+        {
+            "title":"98%",
+            "des": "Client satisfied"
+        },
+        {
+            "title":"80%",
+            "des": "Design Project"
+        }
+    ]
+  return (
+    <div className="pt-[35px] pb-[27px]">
+      <div className="max-w-[1374px] mx-auto flex items-center justify-between flex-col lg:flex-row">
+        <div className="max-w-[50%]">
+          <Image src={about} alt="suleiman about" />
+        </div>
+        <div className="max-w-[50%]">
+          <p className="text-base text-[#1C1C57] pb-4">About Me</p>
+          <h3 className="text-[40px] font-medium text-[#1C1C57] leading-[120%]">
+            Who is responsible for all of this impressive effort?
+          </h3>
+          <p className="text-base text-[#1C1C57] mt-6">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique. Duis cursus, mi quis viverra ornare, eros dolor interdum nulla, ut commodo diam libero vitae erat.
+          </p>
+          <div className="mt-8 mb-10 flex flex-col sm:flex-row gap-14 items-stretch justify-start">
+            {srslist.map((list)=> <div key={list?.des} className="bg-[#FDFDFF] rounded-lg p-3 srsDropAbout">
+                <h2 className="text-[36px] text-[#1C1C57] font-medium">{list?.title}</h2>
+                <p className="text-base text-[#1C1C57] mt-2">{list?.des}</p>
+            </div>)}
+          </div>
+          <ButtonFull text={"About"}/>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AboutMe;
--- app/page.js
+++ app/page.js
@@ -1,3 +1,4 @@
+import AboutMe from "./Components/About Me/AboutMe";
 import HeroArea from "./Components/HeroArea";
 import OurService from "./Components/Our Service/OurService";
 import TrustedComapny from "./Components/TrustedCompany/TrustedComapny";
@@ -17,6 +18,7 @@ export default function Home() {
       >
         <path d="M0 129.842V0.568253H1440L0 129.842Z" fill="#F6F8FF" />
       </svg>
+      <AboutMe/>
     </>
   );
 }
