31f81862f718ff42bcb0a4aa5d7239eb9e9d0cfc
unknown <sakibsafat47@gmail.com>
2024-06-30 01:23:04 -0700

    card dynamically imleplemtation

--- app/Components/My Experience/ExperienceCard.jsx
+++ app/Components/My Experience/ExperienceCard.jsx
@@ -1,19 +1,19 @@
 import Image from "next/image";
 import React from "react";
 
-const ExperienceCard = () => {
+const ExperienceCard = ({icon, companyName, title, year, description}) => {
   return (
     <div className="bg-white rounded-2xl text-[#1C1C57] shadow-sm p-4">
       <div className="flex items-center gap-4">
-        <Image src="/facebook.svg" alt="facebook" width={48} height={48}/>
+        <Image src={`${icon}`} alt="facebook" width={48} height={48}/>
 
         <p className="font-bold text-lg">
-            Facebook
+            {companyName}
         </p>
       </div>
-      <p className="text-xl font-medium mt-4 mb-2">Webflow developments</p>
-      <p className="mb-4">May 12 2023 Present</p>
-      <p className="text-[#3E3E70]">All data is encrypted to ensure the confidentiality and integrity of your portfolio.</p>
+      <p className="text-xl font-medium mt-4 mb-2">{title}</p>
+      <p className="mb-4">{year}</p>
+      <p className="text-[#3E3E70]">{description}</p>
     </div>
   );
 };
--- app/Components/My Experience/MyExperience.jsx
+++ app/Components/My Experience/MyExperience.jsx
@@ -4,7 +4,43 @@ import experience from "../../../public/my experience.png"
 import ExperienceCard from "./ExperienceCard";
 
 
+
+
 const MyExperience = () => {
+
+  const experienceList=[
+    {
+      company:"Facebook",
+      icon:"/facebook.svg",
+      title:"Webflow developments",
+      year:"May 12 2023 Present",
+      description:"All data is encrypted to ensure the confidentiality and integrity of your portfolio.",
+    },
+    {
+      company:"Twitter",
+      icon:"/facebook.svg",
+      title:"UI/UX Designer",
+      year:"June 18 2022 -Dec 2023",
+      description:"All data is encrypted to ensure the confidentiality and integrity of your portfolio.",
+    },
+    {
+      company:"Slack",
+      icon:"/facebook.svg",
+      title:"Head of Product Design",
+      year:"June 18 2021 -August 2023",
+      description:"All data is encrypted to ensure the confidentiality and integrity of your portfolio.",
+    },
+    {
+      company:"Figma",
+      icon:"/facebook.svg",
+      title:"Product Designer",
+      year:"June 18 2020 -Dec 2022",
+      description:"All data is encrypted to ensure the confidentiality and integrity of your portfolio.",
+    },
+
+  ]
+
+
   return (
     <div className='pt-[35px] pb-[27px] bg-[#F6F8FF]'>
       <div className="max-w-[1374px] mx-auto flex items-center justify-between flex-col lg:flex-row-reverse">
@@ -20,7 +56,10 @@ const MyExperience = () => {
           At Airfolio, we prioritize the security of your portfolio. With advanced encryption and secure servers, you can trust that your work is safe with us.
           </p>
           <div className="mt-8 mb-10 grid grid-cols-1 lg:grid-cols-2 gap-14 items-stretch justify-start">
-            <ExperienceCard/>
+            {experienceList.map((item=>(
+              <ExperienceCard key={item?.company} icon={item?.icon} companyName={item?.company} title={item?.title} year={item?.year} description={item?.description} />
+            )))}
+            
           </div>
         </div>
       </div>
