e219c7f5cc95d3e3846033fc0e7b05621ca43e7f
unknown <sakibsafat47@gmail.com>
2024-07-02 20:38:47 +0600

    blog sec implementation

---
+++ app/Components/Blog/Blog.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Blog = () => {
+    return (
+        <div className='max-w-[1320px] mx-auto my-[112px]'>
+             <p className="text-base text-[#1C1C57] pb-4 text-center font-semibold">Inspiration</p>
+            <h3 className="text-[40px] font-semibold text-[#1C1C57] leading-[120%] text-center">
+            Discover the Design World
+            </h3>
+            <p className="text-base font-medium text-[#1C1C57] text-center mt-6">
+            Stay updated with the latest trends and insights in the design industry.
+            </p>
+        </div>
+    );
+};
+
+export default Blog;
