24a42cb23dc5c5561890897d5db3eaec12131e0f
unknown <sakibsafat47@gmail.com>
2024-07-16 23:01:44 +0600

    testimonial svg

--- app/page.js
+++ app/page.js
@@ -1,4 +1,5 @@
 import AboutMe from "./Components/About Me/AboutMe";
+import Blog from "./Components/Blog/Blog";
 import HeroArea from "./Components/HeroArea";
 import MyExperience from "./Components/My Experience/MyExperience";
 import OurService from "./Components/Our Service/OurService";
@@ -40,7 +41,27 @@ export default function Home() {
       >
         <path d="M0 129.842V0.568253H1440L0 129.842Z" fill="#F6F8FF" />
       </svg>
-      <Project/>
+      <Project />
+      {/* <Blog/> */}
+      <svg
+        width="2040"
+        height="130"
+        viewBox="0 0 1440 130"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path d="M1440 0.390137V129.664H0L1440 0.390137Z" fill="#255BF9" />
+      </svg>
+
+      <svg
+        width="2040"
+        height="130"
+        viewBox="0 0 1440 130"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path d="M0 129.827V0.552628H1440L0 129.827Z" fill="#255BF9" />
+      </svg>
     </>
   );
 }
