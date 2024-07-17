a408446b342b1e4055c76c03e88932e412f7c189
unknown <sakibsafat47@gmail.com>
2024-07-18 00:17:46 +0600

    hero image change

--- app/Components/HeroArea.jsx
+++ app/Components/HeroArea.jsx
@@ -8,7 +8,7 @@ const HeroArea = () => {
         <div className="srsHeroParent pt-28 px-5">
             <div className="srsContentParent max-w-[1280px] flex-col lg:flex-row flex mx-auto items-center justify-center gap-5 lg:gap-20">
                 <div className="srsTextContent pb-20 flex flex-col gap-6 max-w-[584px]">
-                    <p className="text-base text-[#1C1C57]">Hi there,My name is Jane Cooper</p>
+                    <p className="text-base text-[#1C1C57]">Hi there,My name is MD Sulyman Ali</p>
                     <h2 className="text-[56px] text-[#1C1C57]">Showcase Your Work with Airfolio's Creative Solutions</h2>
                     <p className="text-base text-[#1C1C57]">Create stunning portfolios and websites with Airfolio's UI/UX design and Webflow development services.</p>
                     <div className="flex items-center  gap-4">
