b2f2860e0e8cba4f7e3b9cae2f0a3dcc744a781d
unknown <sakibsafat47@gmail.com>
2024-07-17 15:14:04 +0600

    padding issue team sec

--- app/Components/Our Team/OurTeamCard.jsx
+++ app/Components/Our Team/OurTeamCard.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 
 const OurTeamCard = ({name, img, title}) => {
     return (
-        <div className='flex mt-10 items-center justify-center gap-3 p-2 h-20 rounded-md bg-white mx-5'>
+        <div className='flex mt-10 items-center justify-center gap-3 p-4 h-20 rounded-md bg-white mx-5'>
                     <Image src={img} className='rounded-md' width={50} height={50}/>
                     <div>
                         <p className='text-xl font-bold mb-1'>{name}</p>
